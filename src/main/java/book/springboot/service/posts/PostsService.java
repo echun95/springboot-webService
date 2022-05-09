fd6b7a8882e059b40419d1ac3e2cb186f2b4ab0b
leeyoungchun <dldydcns123@gmail.com>
2022-05-09 23:29:27 +0900

    Posts 엔티티 생성 및 controller, service, Spring Data JPA Repository 생성, dto 생성, save, findById, findAll, update 구현 및 테스트

---
+++ src/main/java/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,33 @@
+package book.springboot.service.posts;
+
+import book.springboot.web.domain.posts.Posts;
+import book.springboot.web.domain.posts.PostsRepository;
+import book.springboot.web.dto.PostsResponseDto;
+import book.springboot.web.dto.PostsSaveRequestDto;
+import book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        return new PostsResponseDto(entity);
+    }
+}
--- src/test/java/book/springboot/web/HelloControllerTest.java
+++ src/test/java/book/springboot/web/HelloControllerTest.java
@@ -3,16 +3,23 @@ package book.springboot.web;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.hamcrest.core.Is.is;
+
+
+import java.util.regex.Matcher;
 
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
+    @Autowired
     private MockMvc mvc;
 
     @Test
@@ -27,5 +34,20 @@ public class HelloControllerTest {
         //then
       }
 
+      @Test
+      public void helloDto가_리턴된다() throws Exception  {
+          //given
+          String name = "hello";
+          int amount = 1000;
 
+          //when
+          mvc.perform(MockMvcRequestBuilders.get("/hello/dto")
+                  .param("name", name)
+                  .param("amount",String.valueOf(amount)))
+                  .andExpect(MockMvcResultMatchers.status().isOk())
+                  .andExpect(jsonPath("$.name",is(name)))
+                  .andExpect(jsonPath("$.amount", is(amount)));
+
+          //then
+      }
 }
