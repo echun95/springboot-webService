e98635debc44b6e8cdc3f4999487233dd124cb11
leeyoungchun <dldydcns123@gmail.com>
2022-05-10 23:24:41 +0900

    게시판 CRUD 화면 개발 및 api 연동

--- src/main/java/book/springboot/service/posts/PostsService.java
+++ src/main/java/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package book.springboot.service.posts;
 
 import book.springboot.web.domain.posts.Posts;
 import book.springboot.web.domain.posts.PostsRepository;
+import book.springboot.web.dto.PostsListResponseDto;
 import book.springboot.web.dto.PostsResponseDto;
 import book.springboot.web.dto.PostsSaveRequestDto;
 import book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,5 +33,18 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        postsRepository.delete(posts);
     }
 }
---
+++ src/test/java/book/springboot/web/IndexControllerTest.java
@@ -0,0 +1,27 @@
+package book.springboot.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩() throws Exception  {
+        //given
+        String body = this.restTemplate.getForObject("/", String.class);
+        //when
+
+        //then
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
+}
