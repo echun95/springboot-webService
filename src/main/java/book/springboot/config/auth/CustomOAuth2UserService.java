ba09b40a73f23eda2f7f27aecd16fdb3799acfdc
leeyoungchun <dldydcns123@gmail.com>
2022-05-11 22:29:48 +0900

    Spring security 설정, OAuth2 설정

---
+++ src/main/java/book/springboot/config/auth/CustomOAuth2UserService.java
@@ -0,0 +1,52 @@
+package book.springboot.config.auth;
+
+import book.springboot.config.auth.dto.OAuthAttributes;
+import book.springboot.config.auth.dto.SessionUser;
+import book.springboot.web.domain.user.User;
+import book.springboot.web.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();  //1 현재 로그인 진행 중인 서비스를 구분하는 코드 (구글, 네이버, 카카오 등)
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
+                .getUserInfoEndpoint().getUserNameAttributeName();  //2 OAuth2 로그인 진행 시 키가 되는 필드값, 구글 기본 코드 : sub, (네이버, 카카오는 기본 지원 x)
+
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes()); //3 OAuth@UserService를 통해 가져온 OAuth2User의 attribute를 담은 클래스
+        User user = saveOrUpdate(attributes);
+        httpSession.setAttribute("user", new SessionUser(user));    //4 세션에 사용자 정보를 저장하기 위한 dto 클래스
+
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
+                attributes.getAttributes(),
+                attributes.getNameAttributeKey()
+        );
+    }
+
+    private User saveOrUpdate(OAuthAttributes attributes){
+        User user = userRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                .orElse(attributes.toEntity());
+        return userRepository.save(user);
+    }
+}
