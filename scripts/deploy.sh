57798a8b5edbf3126a754c2dd7b0550b61e242a0
leeyoungchun <dldydcns123@gmail.com>
2022-05-21 20:03:19 +0900

    pgrep 설정 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl springboot-webService | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl springboot-webService)
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
@@ -21,6 +21,7 @@ fi
 
 echo "> 새 애플리케이션 배포"
 
+# shellcheck disable=SC2012
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
